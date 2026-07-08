33261e8573fff48b537140edf07542e07267d186
Sachin-840480 <sachin1712003@gmail.com>
2026-07-08 07:07:23 +0530

    Updated bat and shell scripts

--- start_servers.bat
+++ start_servers.bat
@@ -1,4 +1,4 @@
 #Start Both Flask as well as React Servers at the Same Time.
 
 #Start the Streamlit Starting Script.
-wt -w 0 powershell -noExit -Command "& 'D:\VS Code Programs\Python\gtd\start_streamlit.ps1'"
+wt -w 0 powershell -noExit -Command "& 'D:\VS Code Programs\Python\projects\gtd\start_streamlit.ps1'"
--- start_streamlit.ps1
+++ start_streamlit.ps1
@@ -3,12 +3,10 @@ cd "D:\VS Code Programs\Python"
 .venv\Scripts\Activate
 
 # Change the Directory to the Place where 'Server' file is Located.
-cd "gtd"
+cd "projects\gtd"
+
+# Set Streamlit Configuration directory.
+set STREAMLIT_CONFIG_DIR= "D:\VS Code Programs\Python\projects\gtd\.streamlit"
 
 # Start the Server.
-
-set STREAMLIT_CONFIG_DIR= "D:\VS Code Programs\Python\gtd\.streamlit"
 streamlit run gtd_dashboard_final.py
-
-
-
