cb9b469af01f7638c8fbfe115f309d82e5383c78
Sachin-840480 <sachin1712003@gmail.com>
2026-07-16 12:33:00 +0530

    Added Footer to Each Page

---
+++ src/utils/helpers.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def page_footer():
+    st.divider()
+
+    st.caption(
+        "Built with Streamlit, Folium, Matplotlib & XGBoost • Data Source: Global Terrorism Database (GTD)"
+    )
